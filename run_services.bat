1b7b614d1aa1f9faf4880f3750ae6edb80d42ec6
Zakir Aghayev <agayevzakir239@gmail.com>
2024-06-02 13:01:39 +0400

    done

--- run_services.bat
+++ run_services.bat
@@ -4,7 +4,7 @@ REM Navigate to the BACKEND directory
 cd managements
 
 REM Run the backend server in a minimized PowerShell window
-start "DJANGO" powershell -windowstyle minimized -command "cd /d %CD%; .\venv\Scripts\activate; python manage.py migrate; Start-Process powershell -ArgumentList 'python manage.py runserver 0.0.0.0:8000' -NoNewWindow"
+start "DJANGO" powershell -windowstyle minimized -command "cd /d %CD%; .\venv\Scripts\activate; python manage.py migrate; python manage.py runserver 0.0.0.0:8000"
 
 REM Navigate back to the main folder
 cd ..
@@ -13,7 +13,7 @@ REM Navigate to the FRONTEND directory
 cd frontend
 
 REM Run the frontend server in a minimized PowerShell window
-start "FRONT" powershell -windowstyle minimized -command "cd /d %CD%; Start-Process powershell -ArgumentList 'npm run serve' -NoNewWindow"
+start "FRONT" powershell -windowstyle minimized -command "cd /d %CD%; npm run serve"
 
 REM Navigate back to the main folder
 cd ..
@@ -22,7 +22,7 @@ REM Navigate to the PRINTER directory
 cd printer-v2
 
 REM Run the printer service in a minimized PowerShell window
-start "PRINTER" powershell -windowstyle minimized -command "cd /d %CD%; Start-Process powershell -ArgumentList 'npm start' -NoNewWindow"
+start "PRINTER" powershell -windowstyle minimized -command "cd /d %CD%; npm start"
 
 REM Navigate back to the main folder
 cd ..
